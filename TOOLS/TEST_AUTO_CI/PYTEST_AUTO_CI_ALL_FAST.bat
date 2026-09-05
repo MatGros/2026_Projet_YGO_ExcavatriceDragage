0cb63689bb428684685b1a966971eb31dcbc5ca0
Mathieu Gros <mathieu.gros@gmail.com>
2026-09-05 22:54:25 +0200

    wip: sauvegarde avant nettoyage DOC/WFLOW (Option 3) [WIP]

--- TOOLS/TEST_AUTO_CI/PYTEST_AUTO_CI_ALL_FAST.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-pushd "%~dp0..\.."
-python -m pytest TOOLS/TEST_AUTO_CI/TEST_AUTO_CI_ALL -s -rP
-popd
-
--- TOOLS/TEST_AUTO_CI/PYTEST_AUTO_CI_UNITARY_FAST.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-pushd "%~dp0..\.."
-python -m pytest TOOLS/TEST_AUTO_CI/TEST_AUTO_CI_UNITARY -s -rP
-popd
-
